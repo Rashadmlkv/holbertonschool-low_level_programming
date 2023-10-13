3e743ebb49fdeaa5d2479f645a7067b7bc1b3d2c
Rashadmlkv <resadmlkv@gmail.com>
2023-10-13 10:49:18 -0700

    seperated text into lines

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -3,10 +3,16 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always 1 (Success in that program)
  */
 int main(void)
 {
-	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61, 1, stderr);
+	/**
+	 *
+	 * Print text, 59 charcs long, 1 string, as error output
+	 *
+	 */
+	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	59, 1, stderr);
 	return (1);
 }
