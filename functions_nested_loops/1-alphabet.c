723276fbb59faa1a3931ca51808dc051e2cf3586
Rashadmlkv <resadmlkv@gmail.com>
2023-10-16 02:08:25 -0700

    uploading

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * description: print lowercase letters
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
+
+void print_alphabet(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		_putchar(letter);
+		letter++;
+	}
+	_putchar('\n');
+}
