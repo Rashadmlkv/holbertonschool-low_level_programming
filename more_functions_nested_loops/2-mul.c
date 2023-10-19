69ba45054c3b4524f4025d8d62905cafb6fbe97a
Rashadmlkv <resadmlkv@gmail.com>
2023-10-19 11:28:07 -0700

    uploading

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - multiply
+ * @a: operand
+ * @b:  operand
+ * Description: Multiply given argument
+ * Return: Result
+ */
+int mul(int a, int b)
+{
+	return (a * b); }
