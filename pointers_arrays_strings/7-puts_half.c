3a9c68c80999cda6d67c7f69251d84a780951faf
Rashadmlkv <resadmlkv@gmail.com>
2023-10-23 04:52:02 -0700

    first commit

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts_half -print half of string
+ * @str: string
+ */
+void puts_half(char *str)
+{
+	int base = 0, limit = 0;
+
+	while (str[limit] != '\0')
+	{
+		limit++; }
+	if (limit % 2 != 0)
+		base = (limit - 1) / 2;
+	else
+		base = (limit + 1) / 2;
+	for (; base < limit + 1; base++)
+	{
+		_putchar(str[base]); }
+	_putchar('\n');
+}
