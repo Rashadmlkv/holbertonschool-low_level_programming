380d326d694c7cf0e1506b13340825a15234b289
Rashadmlkv <resadmlkv@gmail.com>
2023-10-23 02:02:44 -0700

    first commit

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * reset_to_98 - reset value to 98 by pointer
+ * @n: pointer to integer
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This repo is for practicing: pointers, arrays and data structures
