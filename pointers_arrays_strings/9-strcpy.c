e4fcc85bcc4af418c3344f52b437ce1dabef6e28
Rashadmlkv <resadmlkv@gmail.com>
2023-10-23 14:55:10 -0700

    first commit

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strcpy - copy string to array
+ * @dest: array
+ * @src: string
+ * Return: copied array
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0, j;
+
+	while (src[i] != '\0')
+		i++;
+
+	for (j = 0; j <= i; j++)
+		*(dest + j) = src[j];
+
+	dest[i + 1] += '\0';
+	return (dest);
+}
