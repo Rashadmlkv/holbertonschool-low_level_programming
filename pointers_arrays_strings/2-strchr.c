7830510837f5845938e0d7ce203ceae8baf0a3b1
Rashadmlkv <resadmlkv@gmail.com>
2023-10-26 01:02:26 -0700

    first commit

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character in a string.
+ * @s: string
+ * @c: character
+ * Return:  pointer to the first occurrence of c in s, or NULL if not found
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+	return ('\0');
+}
