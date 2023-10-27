fb46f46ca177a2e128751197391165517c1fe381
Rashadmlkv <resadmlkv@gmail.com>
2023-10-27 01:14:52 -0700

    upload

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -17,7 +17,7 @@ char *_strncat(char *dest, char *src, int n)
 		dest++;
 	}
 	for (i = 0 ; i < n && src[i] != '\0' ; i++)
-		*(dest + i) = src[i];
+		dest[i] = src[i];
 	dest[i+ 1] = '\0';
 	return (dest);
 }
