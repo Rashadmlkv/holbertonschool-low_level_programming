c5981e7f3e66146f1eafbd98be8b98ecf0815f79
Rashadmlkv <resadmlkv@gmail.com>
2023-10-27 02:25:13 -0700

    first commit

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings.
+ * @s1: 1st string
+ * @s2: 2nd
+ * Return: Neg is lower, Pos if higher, 0 if equal
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0, j = 0;
+
+	while (s1[i] != '\0')
+	{
+		i++;
+	}
+	while (s2[j] != '\0')
+	{
+		j++;
+	}
+
+	if (i < j)
+		return (-1);
+	if (i > j)
+		return (1);
+	return (0);
+}
