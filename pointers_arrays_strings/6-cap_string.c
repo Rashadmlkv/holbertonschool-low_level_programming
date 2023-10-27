d5521138dd9fd7897d1d13a71d1d7c4778e1641e
Rashadmlkv <resadmlkv@gmail.com>
2023-10-27 05:34:28 -0700

    first upload

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalize low cases of start of words
+ * @a: string
+ * Return: pointer to dest
+ */
+char *cap_string(char *a)
+{
+	int i;
+
+	for (i = 0; a[i] != '\0'; i++)
+	{
+		if (a[i] >= 'a' && a[i] <= 'z')
+		{
+			if (a[i - 1] >= '\t' && a[i - 1] <= '/')
+				a[i] = a[i] - 32; }
+	}
+	return (a);
+}
