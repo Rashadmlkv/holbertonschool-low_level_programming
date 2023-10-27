1bb94ac25bd0ae2d6106f2f0b9bc0f6744c41948
Rashadmlkv <resadmlkv@gmail.com>
2023-10-27 01:02:13 -0700

    uploading

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -12,8 +12,12 @@ char *_strncat(char *dest, char *src, int n)
 {
 	int i;
 
+	while (*dest != '\0')
+	{
+		dest++;
+	}
 	for (i = 0 ; i < n && src[i] != '\0' ; i++)
-		dest[i] = src[i];
-	dest[i] = '\0';
+		*(dest + i) = src[i];
+	dest[i+ 1] = '\0';
 	return (dest);
 }
