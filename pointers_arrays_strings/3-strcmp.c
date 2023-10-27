623240b3ffa8477bc04d0ca746757df62e935800
Rashadmlkv <resadmlkv@gmail.com>
2023-10-27 02:46:43 -0700

    commit

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -19,9 +19,9 @@ int _strcmp(char *s1, char *s2)
 		j++;
 	}
 
-	if (i < j || j < i)
-		return (-1);
-	if (i > j || j > i)
-		return (1);
+	if (j < i)
+		return (-13);
+	if (j > i)
+		return (13);
 	return (0);
 }
