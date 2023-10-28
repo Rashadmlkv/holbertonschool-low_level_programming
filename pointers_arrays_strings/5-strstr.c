139f680eac82c8ff39b5f2bd9d61150962e394c8
Rashadmlkv <resadmlkv@gmail.com>
2023-10-28 07:44:45 -0700

    added null input option, fixed needle search as it was not looking whole needle

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -9,20 +9,28 @@
 char *_strstr(char *haystack, char *needle)
 {
 	int i = 0, c = 1;
-	char **ptr;
+	char *ptr;
 
+	if (*needle == '\0')
+		return (haystack);
 	while (*haystack != '\0')
 	{
-		ptr = &haystack;
-		if (**ptr == needle[i] && needle[i] != '\0')
+		if (*haystack == *needle)
 		{
-			i++;
-			c = 1; }
-		else
-		{
-			c = 0; }
-		if (c == 1)
-			return (haystack);
+			ptr = haystack;
+			while (needle[i] != '\0')
+			{
+				if (*ptr == needle[i])
+					c = 1;
+				else
+					c = 0;
+				i++;
+				ptr++;
+			}
+			i = 0;
+			if (c == 1)
+				return (haystack);
+		}
 		haystack++;
 	}
 	return ('\0');
