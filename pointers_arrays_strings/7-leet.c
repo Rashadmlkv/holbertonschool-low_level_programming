5265e494386c79d9f2df9d124e4eec9c9a1ae1a8
Rashadmlkv <resadmlkv@gmail.com>
2023-10-29 11:32:32 -0700

    first commit

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * leet -  encodes a string into 1337.
+ * @a: string
+ * Return: leet string
+ */
+char *leet(char *a)
+{
+	char arr[5] = {'a', 'e', 'o', 't', 'l'};
+	char num[5] = {'4', '3', '0', '7', '1'};
+	int i, flag;
+
+	for (; *a != '\0'; a++)
+	{
+		flag = 1;
+		for (i = 0; arr[i] != '\0' && flag == 1; i++)
+		{
+			if (*a == arr[i] || *a == (arr[i] - 32))
+			{
+				*a = num[i];
+				flag = 0; } }
+	}
+	return (a);
+}
