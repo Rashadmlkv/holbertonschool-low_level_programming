3681bc3b20506da7d81c08aeba8be2a7959c8d8a
Rashadmlkv <resadmlkv@gmail.com>
2023-10-31 23:27:18 -0700

    added description

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,9 +1,15 @@
 #include "main.h"
 #include <stdio.h>
 
-int main(int argc, char* argv[])
+/**
+ * main - prints its name
+ * @argc: unused
+ * @argv: name
+ * Return: 0
+ */
+int main(int argc, char *argv[])
 {
 	(void)argc;
 	printf("%s\n", argv[0]);
-	return(0);
+	return (0);
 }
