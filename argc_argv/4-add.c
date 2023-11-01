bd94e6d48eb542d13c9821792e07da5c87aa5400
Rashadmlkv <resadmlkv@gmail.com>
2023-11-01 04:03:02 -0700

    first commit

---
+++ argc_argv/4-add.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main -  adds positive numbers.
+ * @argc: counter
+ * @argv: string
+ * Return: 0 success, -1 error
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	int result = 0;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0); }
+	for (i = 1; i < argc; i++)
+	{
+		if (*argv[i] < '0' || *argv[i] > '9')
+		{
+			printf("Error\n");
+			return (1); }
+	}
+	for (i = 1; i < argc; i++)
+		result += atoi(argv[i]);
+	printf("%d\n", result);
+	return (0);
+}
