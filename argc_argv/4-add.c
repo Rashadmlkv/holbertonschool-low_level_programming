ed3c4f1b8fb2a1e8e15420a41fb9843fd2306baf
Rashadmlkv <resadmlkv@gmail.com>
2023-11-01 04:44:58 -0700

    upload

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -17,9 +17,10 @@ int main(int argc, char *argv[])
 	{
 		printf("0\n");
 		return (0); }
+
 	for (i = 1; i < argc; i++)
 	{
-		if (argv[i][1] < '0' || argv[i][1] > '9')
+		if ((argv[i][1] < '0' || argv[i][1] > '9') && argv[i][1] != '\0')
 		{
 			printf("Error\n");
 			return (1); }
