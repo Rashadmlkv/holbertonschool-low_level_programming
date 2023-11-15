683c0c70a5b7cec8664a18580cf68ea5161b16d3
Rashadmlkv <resadmlkv@gmail.com>
2023-11-15 01:48:34 -0800

    first commit

---
+++ preprocessor/02-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#ifndef __FILE__
+#define __FILE__
+#endif
+
+/**
+ * main - print the file compiled from
+ * Return: 0
+ */
+int main(void)
+{
+	printf(__FILE__ "\n");
+	return (0);
+}
