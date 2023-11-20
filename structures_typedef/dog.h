3e3d9616e20bc8a2f982a973b35f95502d1eab64
Rashadmlkv <resadmlkv@gmail.com>
2023-11-20 00:27:52 -0800

    first commit

---
+++ structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+/**
+ * struct dog - type
+ * @name: name
+ * @owner: owner
+ * @age: age
+ */
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+#endif
