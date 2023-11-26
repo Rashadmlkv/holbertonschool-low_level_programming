3ffb77d9a374f6bdb41d5cef82e2a8c2d543742e
Rashadmlkv <resadmlkv@gmail.com>
2023-11-26 04:49:54 -0800

    first commit

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,24 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - count the sum of all nums
+ * @n: count of given arguments
+ * Return - sum if success, 0 if not
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i, sum;
+
+	if (n == 0)
+		return (0);
+
+	va_start(ap, n);
+
+	sum = 0;
+	for (i = 0; i < n; i++)
+		sum += va_arg(ap, unsigned int);
+
+	va_end(ap);
+	return (sum);
+}
