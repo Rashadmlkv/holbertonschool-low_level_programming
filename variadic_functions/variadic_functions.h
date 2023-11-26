22081e4bcc8b03cc7764e617be24ff72e67b2882
Rashadmlkv <resadmlkv@gmail.com>
2023-11-26 04:52:48 -0800

    fixed double inclusion

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,9 +1,8 @@
+#ifndef _VARIADIC_FUNCTIONS_H_
+#define _VARIADIC_FUNCTIONS_H_
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
-
-#ifndef _VARIADIC_FUNCTIONS_H_
-#define _VARIADIC_FUNCTION_H_
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
