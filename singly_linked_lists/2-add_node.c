5e06e5b169da9c24347d319a77cdffea0f8c5140
Rashad <58619029+Rashadmlkv@users.noreply.github.com>
2023-12-05 14:37:46 +0400

    added failing conditions

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -11,8 +11,12 @@ list_t *add_node(list_t **head, const char *str)
 	list_t *temp;
 
 	temp = malloc(sizeof(list_t));
+	if (!temp)
+		return (NULL);
 	temp->next = *head;
 	temp->str = strdup(str);
+	if (!temp->str)
+		return (NULL);
 	temp->len = strlen(str);
 	head = &temp;
 	return (*head);
