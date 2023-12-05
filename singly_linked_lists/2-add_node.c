ee14e5fd22a1ec5c6435041e96969e0da1a1dbbf
Rashad <58619029+Rashadmlkv@users.noreply.github.com>
2023-12-05 14:43:30 +0400

    Update 2-add_node.c

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -12,12 +12,24 @@ list_t *add_node(list_t **head, const char *str)
 
 	temp = malloc(sizeof(list_t));
 	if (!temp)
+	{
+		free(temp->str);
+		free(temp->len);
+		free(temp->next);
+		free(temp);
 		return (NULL);
+	}
 	temp->next = *head;
+	temp->len = strlen(str);
 	temp->str = strdup(str);
 	if (!temp->str)
+	{
+		free(temp->str);
+		free(temp->len);
+		free(temp->next);
+		free(temp);
 		return (NULL);
-	temp->len = strlen(str);
-	head = &temp;
+	}
+	*head = &temp;
 	return (*head);
 }
