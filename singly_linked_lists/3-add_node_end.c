a06606cc8acf2c23f75a5694e4781a972550185f
Rashadmlkv <resadmlkv@gmail.com>
2023-12-06 02:12:05 -0800

    first commit

---
+++ singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+
+/**
+ * add_node_end - add a new node to the list
+ * @head: head pointer
+ * @str: data of node
+ * Return: pointer to new node
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *tmp, *newnode;
+
+	newnode = malloc(sizeof(list_t));
+
+	if (newnode)
+	{
+		newnode->str = strdup(str);
+		newnode->len = strlen(str);
+		newnode->next = NULL;
+
+		if (*head == NULL)
+		{
+			*head = newnode;
+		}
+		else
+		{
+			while (tmp->next != NULL)
+				tmp = tmp->next;
+
+			tmp->next = newnode;
+		}
+		return (newnode);
+	}
+	return (NULL);
+}
