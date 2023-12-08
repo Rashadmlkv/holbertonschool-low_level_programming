8bfd6c69b845ef68ae04ef6af13591694de03e69
Rashadmlkv <resadmlkv@gmail.com>
2023-12-08 06:04:22 -0800

    first commit

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - frees linked list
+ * @head: head of list
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *tmp;
+
+	while (head != NULL)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp);
+	}
+}
