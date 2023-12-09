1c849fcd6ad461219fb4fd770e0d07b892157672
Rashadmlkv <resadmlkv@gmail.com>
2023-12-09 00:45:11 -0800

    first commit

---
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+/**
+ * delete_dnodeint_at_index - deletes the node at index of a  linked list.
+ * @head: start point of list
+ * @index: end point
+ * Return: 1 if it succeeded, -1 if it failed
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	unsigned int idx;
+	dlistint_t *tmp = *head;
+
+	if (*head == NULL)
+		return (-1);
+
+	for (idx = 0; tmp != NULL; idx++)
+	{
+		if (idx == 0)
+		{
+			*head = tmp->next;
+			free(tmp);
+			return (1); }
+
+		else if (idx == index)
+		{
+			tmp->prev->next = tmp->next;
+			free(tmp);
+			return (1); }
+		tmp = tmp->next;
+	}
+	return (-1);
+}
