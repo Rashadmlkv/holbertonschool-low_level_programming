01f8b2e0a8b313b4c40885e714e7a63f72fb1374
Rashadmlkv <resadmlkv@gmail.com>
2023-12-11 06:17:13 -0800

    first commit

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <fcntl.h>
+
+/**
+ * read_textfile - reads textfile and prints contents + bytes written
+ * @filename: name of file
+ * @letters: count of bytes
+ * Return: count of bytes written in succes, 0 in failure
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd, tmp;
+	char *strptr = malloc(sizeof(char) * letters + 1);
+
+	if (strptr == NULL || filename == NULL)
+		return (0);
+
+	fd = open(filename, 0);
+	if (fd < 0)
+		return (0);
+	fd = read(fd, strptr, letters);
+	if (fd < 0)
+		return (0);
+	tmp = write(1, strptr, letters);
+	if (tmp < 0)
+		return (0);
+	return (fd);
+}
