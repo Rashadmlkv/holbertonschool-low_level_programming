4f5d7bcf60a3b34b50762c6226d7b19a15a989ad
Rashadmlkv <resadmlkv@gmail.com>
2023-12-17 06:11:52 -0800

    first commit

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -6,7 +6,6 @@
  * @size: size
  * Return: index
  */
-unsigned long int key_index(const unsigned char *key, unsigned long int size);
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	unsigned long int hash = hash_djb2(key);
-	return (hash % size); }
+	return (hash_djb2(key) % size); }
